9bf8bbb062b237daedab73c1ea3232092e983fdf
shunchi <shunchi100@gmail.com>
2018-03-02 23:14:09 -0800

    .gitignore added

--- CrossPy/test/test.py
+++ CrossPy/test/test.py
@@ -1,7 +1,13 @@
 import pytest
-from CrossPy import CrossPy
+import numpy as np
+import pandas as pd
+from CrossPy.CrossPy import crosspy
+
+data = pd.read_csv("./test_data/test_data_short.csv")
+X = data.iloc[]
 
 class Test_train_test_split():
 
     def test_type(self):
-        assert  
+        with pytest.raise(TypeError):
+            crosspy.train_test_split(X = , y =)
