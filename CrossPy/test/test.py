ac4002c8d88d8ca9adc38869f499976cdf4b93c4
shunchi <shunchi100@gmail.com>
2018-03-02 23:22:04 -0800

    test.py: Test_train_test_split() started

--- CrossPy/test/test.py
+++ CrossPy/test/test.py
@@ -4,10 +4,13 @@ import pandas as pd
 from CrossPy.CrossPy import crosspy
 
 data = pd.read_csv("./test_data/test_data_short.csv")
-X = data.iloc[]
+X = data.iloc[:,:-1]
+y = data.iloc[:,-1]
+y_list = list(y)
 
 class Test_train_test_split():
 
     def test_type(self):
         with pytest.raise(TypeError):
-            crosspy.train_test_split(X = , y =)
+            crosspy.train_test_split(X = X, y = y_list)
+
