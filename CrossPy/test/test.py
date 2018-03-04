587a3f459f853b9e4b5308f9df518ff1507ef981
shunchi <shunchi100@gmail.com>
2018-03-04 02:52:47 -0800

    test.py: Added Test_cross_validation()

--- CrossPy/test/test.py
+++ CrossPy/test/test.py
@@ -3,8 +3,12 @@ import numpy as np
 import pandas as pd
 #from CrossPy.CrossPy import crosspy
 import CrossPy
+from sklearn.linear_model import LinearRegression
 
+
+# read test data
 data = pd.read_csv("./test_data/test_data_short.csv")
+# build test data
 X = data.iloc[:,:-1]
 y = data.iloc[:,-1]
 X_matrix = X.as_matrix()
@@ -12,10 +16,13 @@ y_list = list(y)
 X_longer = X.append(X)
 y_2 = pd.concat([y, y], axis=1)
 
+# build a model
+lm = LinearRegression()
+
 
 class Test_train_test_split():
 '''
-Tests for function `train_test_split(self, X, y, test_size = 0.25, shuffle = True, random_state = None)`
+Tests for function `train_test_split(X, y, test_size = 0.25, shuffle = True, random_state = None)`
 '''
 
     # Input Type Errors
@@ -104,8 +111,76 @@ Tests for function `train_test_split(self, X, y, test_size = 0.25, shuffle = Tru
             "`Shuffle = False` doesn't work!"
 
 
+class Test_cross_validation():
+'''
+Tests for function `cross_validation(model, X, y, k = 3, shuffle = TRUE, random_state = None)`
+'''
+
+    # Input Type Errors
+
+    def test_X_as_dataframe(self):
+        with pytest.raises(TypeError('`X` must be a dataframe')):
+            cross_validation(lm, X = X_matrix, y = y)
+
+    def test_y_as_dataframe(self):
+        with pytest.raises(TypeError('`y` must be a dataframe')):
+            cross_validation(lm, X = X, y = y_list)
+
+    def test_k_as_number(self):
+        with pytest.raises(TypeError('`k` must be an integer')):
+            cross_validation(lm, X = X, y = y, k = '3')
+
+    def test_shuffle_as_boolean(self):
+        with pytest.raises(TypeError('`shuffle` must be True or False')):
+            cross_validation(lm, X = X, y = y, shuffle = '1')
+        with pytest.raises(TypeError('`shuffle` must be True or False')):
+            cross_validation(lm, X = X, y = y, shuffle = 1)
+        with pytest.raises(TypeError('`shuffle` must be True or False')):
+            cross_validation(lm, X=X, y=y, shuffle=1.0)
+
+    def test_random_state_as_number(self):
+        with pytest.raises(TypeError('`random_state` must be a number or None')):
+            cross_validation(lm, X = X, y = y, random_state = '10')
 
 
+    # Input Value Errors
+
+    def test_k_range(self):
+        with pytest.raises(TypeError('`k` must be an integer 2 or greater')):
+            cross_validation(lm, X=X, y=y, k = 1)
+        with pytest.raises(TypeError('`k` must be greater than # obs in X and y')):
+            cross_validation(lm, X=X, y=y, k = 40)
+
+
+    def test_random_state_range(self):
+        with pytest.raises(TypeError('`random_state` must be nonnegative')):
+            cross_validation(lm, X=X, y=y, random_state=-10)
+
+
+    # Input Dimension Errors
+
+    def test_X_y_match(self):
+        with pytest.raises(TypeError("dim of `X` doesn't equal dim of `y`")):
+            cross_validation(lm, X=X_longer, y=y)
+
+
+    def test_y_one_column(self):
+        with pytest.raises(TypeError('`y` is more than one feature')):
+            cross_validation(lm, X=X, y=y_2)
+
+
+    def test_X_y_Nrows(self):
+        with pytest.raises(TypeError('sample size is less than 3, too small for CV')):
+            cross_validation(lm, X=X.iloc[0:2, :], y=y.iloc[0:2, :])
+
+
+    # Output Errors
+    cv_scores = cross_validation(lm, X=X, y=y)
+
+    cv_scores_sklearn = np.array([])
+
+    def compare_sklearn():
+        assert max(cv_scores - cv_scores_sklearn) < 0.000001, "results doesn't match sklearn"
 
 
 class summary_cv():
