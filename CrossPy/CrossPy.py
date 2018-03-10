adb4fa4281de2f6ca173ee2d5a9f5355af78f5da
danraff <d.raff@alumni.ubc.ca>
2018-03-10 10:31:20 -0800

    Completed `cross_validation` with additional new tests
    Make sure to check out the code as it'll likely help in making some
    of the other functions (train_test_split)
    
    Also note that I added in a helper function.
    
    All tests pass

--- CrossPy/CrossPy.py
+++ CrossPy/CrossPy.py
@@ -8,14 +8,9 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score
+from sklearn.utils import check_random_state
 
 
-#from CrossPy.test.test import data_gen
-
-#X, y = data_gen()
-
-
-#print(not isinstance(y, pd.DataFrame))
 
 def train_test_split(X, y, test_size = 0.25, shuffle = True, random_state = None):
     '''
@@ -59,13 +54,98 @@ def cross_validation(model, X, y, k = 3, shuffle = True, random_state = None):
     --------
     scores: a vector of validation scores
     '''
+    ## Input Errors
     if not isinstance(X, pd.DataFrame):
         raise TypeError('`X` must be a dataframe')
     if not isinstance(y, pd.DataFrame):
         raise TypeError('`y` must be a dataframe')
-    else:
-        pass
+    if not isinstance(k, int):
+        raise TypeError('`k` must be an integer')
+    if not isinstance(shuffle, bool):
+        raise TypeError('`shuffle` must be True or False')
+    if not (isinstance(random_state, int) or isinstance(random_state, float) or random_state is None):
+        raise TypeError('`random_state` must be a number or None')
+    if not (random_state is None):
+        if random_state <= 0:
+            raise TypeError('`random_state` must be nonnegative')
+    if k < 2 or k > X.shape[0]:
+        raise TypeError('`k` must be an integer 2 or greater, and be less than # obs in X and y')
+    if X.shape[0] != y.shape[0]:
+        raise TypeError("dim of `X` doesn't equal dim of `y`")
+    if y.shape[1] != 1:
+        raise TypeError('`y` is more than one feature')
+    if X.shape[0] < 3:
+        raise TypeError('sample size is less than 3, too small for CV')
+    if type(model) != type(LinearRegression()):
+        raise TypeError('model is not an sklearn LinearRegression model')
 
+
+    # If shuffle is True or False, this code will still run - see helper function below
+    indices = split_data(X, k, shuffle, random_state)
+
+    # Initialize scores output
+    scores = np.arange(k)
+    # For each fold tuple, get the corresponding training and val X and y, then train and score each
+    for i in np.arange(k):
+        ind_train, ind_val = next(indices)
+        X_train = X.iloc[ind_train, :]
+        X_val = X.iloc[ind_val, :]
+        y_train = X.iloc[ind_train, :]
+        y_val = X.iloc[ind_val, :]
+
+        # Fit each model and score with R^2
+        model.fit(X_train, y_train)
+        score = model.score(X_val, y_val)
+        scores[i] = score
+
+    return scores
+
+
+def split_data(X, k=3, shuffle=True, random_state = None):
+    """
+    Helper Function for cross_validaton
+    :param X: dataframe
+    :param k: number of splits
+    :param shuffle: True or False for whether to shuffle indicies
+    :param random_state: default None
+    :return: yields a 2-tuple of np.array indices, the first for validation indicies, the second for training.
+            yields 2-tuple for each k folds (eg: k = 3, would have 3 2-tuples)
+
+    with help from code: https://github.com/scikit-learn/scikit-learn/blob/a24c8b46/sklearn/model_selection/_split.py#L357
+
+    """
+
+    all_indices = np.arange(X.shape[0])
+    n_samples = X.shape[0]
+
+    ## Should initialize what the output will be here.. based on the k there should be indices for X and y rows,
+    ## therefore could have a list of tuples??
+
+    # In order to align with scikit learn - cross referenced: https://github.com/scikit-learn/scikit-learn/blob/a24c8b46/sklearn/model_selection/_split.py#L357
+    # The first n_samples % n_splits folds have size n_samples // n_splits + 1,
+    # other folds have size n_samples // n_splits, where n_samples is the number of samples.
+
+    fold_extra = n_samples % k
+    num_folds = 0
+
+    fold_sizes = (n_samples // k) * np.ones(k, dtype=np.int)
+    fold_sizes[0:fold_extra] += 1
+
+    val_indicies = []
+
+    if shuffle == True:
+        check_random_state(random_state).shuffle(all_indices)
+
+    current = 0
+    for fold in fold_sizes:
+        start, stop = current, current + fold
+        val_indicies.append(all_indices[start:stop])
+        current = stop
+
+    for fold in val_indicies:
+        val = fold
+        other = np.setdiff1d(all_indices, val)
+        yield (val, other)
 
 
 def summary_cv(scores):
