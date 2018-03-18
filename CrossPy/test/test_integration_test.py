0b190f7e65ae0c165b066e7ccadf1e6a38d311e2
shunchi <shunchi100@gmail.com>
2018-03-18 09:32:11 -0700

    More code style modifications

--- CrossPy/test/test_integration_test.py
+++ CrossPy/test/test_integration_test.py
@@ -1,37 +1,39 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
-
 import numpy as np
 import pandas as pd
-from CrossPy.CrossPy import cross_validation, summary_cv
+from CrossPy.CrossPy import train_test_split, cross_validation, summary_cv
 from sklearn.linear_model import LinearRegression
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
 
 
 # integration test
 
-def test_summary_cv_AND_cross_validation_Perfect_linear():
+def test_integration_perfect_linear():
     X, y = data_gen(nrows=100, Non_perfect=False)
-    scores = cross_validation(lm(), X=X, y=y, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    scores = cross_validation(lm(), X=X_train, y=y_train, shuffle=False)
     summary = summary_cv(scores)
-    assert summary['mean'] == 1 and summary['median'] == 1 and summary['sd'] == 0, 'Perfect linear relation test does not give correct summary'
+    assert summary['mean'] == 1 and summary['median'] == 1 and summary[
+        'sd'] == 0, 'Perfect linear relation test does not give correct summary'
 
-def test_summary_cv_AND_cross_validation_NonPerfect_linear():
+
+def test_integration_nonperfect_linear():
     X, y = data_gen(nrows=100, Non_perfect=True)
-    scores = cross_validation(lm(), X=X, y=y, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    scores = cross_validation(lm(), X=X_train, y=y_train, shuffle=False)
     summary = summary_cv(scores)
-    assert summary['mean'] < 1 and summary['median'] < 1 and summary['sd'] > 0, 'Non-perfect linear relation test does not give correct summary'
-
-
-
+    assert summary['mean'] < 1 and summary['median'] < 1 and summary[
+        'sd'] > 0, 'Non-perfect linear relation test does not give correct summary'
 
 
 # Helper function
 
-## Data Generation
-def data_gen(nrows=100, Non_perfect = False):
-    '''
+# Data Generation
+def data_gen(nrows=100, Non_perfect=False):
+    """
     Generate data
 
     input:
@@ -43,21 +45,25 @@ def data_gen(nrows=100, Non_perfect = False):
     ------
     X, a dataframe with nrows and two columns
     y, a dataframe with nrows and one column
-    '''
+    """
     tmp_data = {"X0": range(nrows), "X1": np.random.rand(nrows)}
     X = pd.DataFrame(tmp_data)
 
-    if Non_perfect == False:
+    if not Non_perfect:
         tmp_data = {"y": range(nrows)}
         y = pd.DataFrame(tmp_data)
     else:
-        tmp_data = {"y": X.X0 + X.X1*2 + nrows/20 * np.random.randn(nrows)}
+        tmp_data = {"y": X.X0 + X.X1 * 2 + nrows / 20 * np.random.randn(nrows)}
         y = pd.DataFrame(tmp_data)
-
 
     return X, y
 
-## linear regression model
+
+# linear regression model
 def lm():
+    """
+    create an linearregression object
+    :return: the object
+    """
     lm = LinearRegression()
     return lm
--- CrossPy/test/test_summary.py
+++ CrossPy/test/test_summary.py
@@ -1,68 +1,83 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
-
 import pytest
 import pandas as pd
 from CrossPy.CrossPy import summary_cv
+import sys
+import os
 
-## Tests summary_cv()
-##'Tests for function `summary_cv(scores)
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+
+
+# Tests summary_cv()
+# 'Tests for function `summary_cv(scores)
 
 # Example Data
 def gen_summary():
-    list =  [0.97, 0.96, 0.98, 0.97, 0.95, 0.97]
-    return list
+    list_test = [0.97, 0.96, 0.98, 0.97, 0.95, 0.97]
+    return list_test
+
 
 # Input Errors:
 
 def test_input_as_dataframe():
     with pytest.raises(TypeError):
-        summary_cv(scores = pd.DataFrame(data={'cv_scores':[0.97, 0.96, 0.98, 0.97, 0.95, 0.97]}))
+        summary_cv(scores=pd.DataFrame(data={'cv_scores': [0.97, 0.96, 0.98, 0.97, 0.95, 0.97]}))
+
 
 def test_input_as_tuple():
     with pytest.raises(TypeError):
-        summary_cv(scores = (0.96, 0.97, 0.98, 0.99))
+        summary_cv(scores=(0.96, 0.97, 0.98, 0.99))
+
 
 def test_input_contains_string():
     with pytest.raises(TypeError):
-        summary_cv(scores = [0.96, 0.97, "0.98"])
+        summary_cv(scores=[0.96, 0.97, "0.98"])
+
 
 def test_zero_length_input():
     with pytest.raises(TypeError):
-        summary_cv(scores = [])
+        summary_cv(scores=[])
+
 
 def test_input_contains_negative():
     with pytest.raises(ValueError):
-        summary_cv(scores = [0.96, 0.97, -0.98])
+        summary_cv(scores=[0.96, 0.97, -0.98])
+
 
 def test_input_contains_over_1():
     with pytest.raises(ValueError):
-        summary_cv(scores = [0.96, 0.97, 1.98])
+        summary_cv(scores=[0.96, 0.97, 1.98])
+
 
 # Output Errors
 
 def test_is_dict():
     assert isinstance(summary_cv(gen_summary()), dict)
 
+
 def test_output_length():
     assert len(summary_cv(gen_summary())) == 3
+
 
 def test_mean_is_float():
     assert isinstance(summary_cv(gen_summary())['mean'], float)
 
+
 def test_median_is_float():
     assert isinstance(summary_cv(gen_summary())['median'], float)
+
 
 def test_sd_is_float():
     assert isinstance(summary_cv(gen_summary())['sd'], float)
 
+
 def test_summary_cv_mean():
     assert summary_cv(gen_summary())['mean'] == 0.967
 
+
 def test_summary_cv_median():
     assert summary_cv(gen_summary())['median'] == 0.97
+
 
 def test_summary_cv_sd():
     assert summary_cv(gen_summary())['sd'] == 0.009
